d0cfcdb591fd6d457addb59db498e1bd5e20b89e
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-18 15:05:22 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { earnCoins } = require('../controllers/userController');
 
+// IMPORTANT: POST route
 router.post('/earn', earnCoins);
 
 module.exports = router;
