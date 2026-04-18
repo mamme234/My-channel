b301226d30e7d2872b53de686a1ea9ce96cf80ff
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-18 14:32:44 +0300

    Update notify.js

--- bot/notify.js
+++ bot/notify.js
@@ -1,11 +1,13 @@
 const bot = require('./telegramBot');
 
-const CHANNEL = process.env.CHANNEL;
+const CHANNEL = process.env.CHANNEL; // MUST be @gangs234
 
 function sendToChannel(message) {
   bot.sendMessage(CHANNEL, message, {
     parse_mode: 'HTML'
-  }).catch(err => console.log(err.message));
+  }).catch(err => {
+    console.log("❌ Telegram Error:", err.response?.body || err.message);
+  });
 }
 
 module.exports = sendToChannel;
