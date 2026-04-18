0c1410997a8fdf66a0fa4eeb514af1a09134215b
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-18 15:06:27 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,22 +2,32 @@ const User = require('../models/User');
 const sendToChannel = require('../bot/notify');
 
 exports.earnCoins = async (req, res) => {
-  const { username, coins } = req.body;
+  try {
+    const { username, coins } = req.body;
 
-  let user = await User.findOne({ username });
+    if (!username || !coins) {
+      return res.status(400).json({ error: "Missing data" });
+    }
 
-  if (!user) {
-    user = new User({ username, coins });
-  } else {
-    user.coins += coins;
+    let user = await User.findOne({ username });
+
+    if (!user) {
+      user = new User({ username, coins });
+    } else {
+      user.coins += coins;
+    }
+
+    await user.save();
+
+    // Telegram post
+    sendToChannel(
+      `💰 <b>Earn Update</b>\n👤 ${username}\n🪙 +${coins} coins`
+    );
+
+    res.json({ success: true, user });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
   }
-
-  await user.save();
-
-  // 🔥 Telegram auto post
-  sendToChannel(
-    `💰 <b>Earn Update</b>\n👤 ${username}\n🪙 +${coins} coins`
-  );
-
-  res.json({ success: true, user });
 };
