cf69afd9df258715475f5001a648ed418cd19f0d
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-25 20:41:58 +0300

    Update sponsor.js

--- bot/sponsor.js
+++ bot/sponsor.js
@@ -6,7 +6,7 @@ function postToChannel(message) {
   return bot.sendMessage(CHANNEL, message, {
     parse_mode: 'HTML'
   })
-  .then(() => console.log("✅ Sent to Telegram"))
+  .then(() => console.log("✅ Posted"))
   .catch(err => console.log("❌ Telegram error:", err.message));
 }
 
