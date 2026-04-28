8cd69542a97ddbd5c0b3708beb1a957361d27e62
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-28 15:37:23 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: String,
-  coins: { type: Number, default: 0 }
+  coins: { type: Number, default: 0 },
+  lastTap: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('User', userSchema);
